9d4f31ed7636447e71666fd794be6e9452446cfb
unknown <Muhamad_R@MTPC9018.mitrais.com>
2016-04-27 13:37:00 +0700

    fisrt commit

---
+++ src/WebApplicationDocker/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationDocker.Models
+{
+    [System.ComponentModel.DataAnnotations.Schema.Table("Book")]
+    public class Book
+    {
+        [ScaffoldColumn(false)]
+        public int BookID { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public int Year { get; set; }
+        [Range(1, 500)]
+        public decimal Price { get; set; }
+
+        public string Genre { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int AuthorID { get; set; }
+
+        // Navigation property
+        public virtual Author Author { get; set; }
+    }
+}
